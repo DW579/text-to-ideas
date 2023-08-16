db673d1d4d129ae70e2a084c0bd2fcb40ca754ba
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-08-16 11:15:06 -0700

    Initial page routing set up

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>React Bootstrap NextJS Tutorial</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aliquid quia optio odit nihil voluptatibus soluta labore earum nostrum doloremque. Sequi laboriosam dicta praesentium, sit aspernatur non molestiae voluptates beatae.</p>
-                    <Button>My Button</Button>
-                </Col>
-            </Row>
-        </Container>
+        <Navbar
+            expand="lg"
+            className="bg-body-tertiary"
+        >
+            <Container>
+                <Navbar.Brand href="#home">Text-to-Ideas</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {/* <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link> */}
+                        <Link href={"/about"}>About</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
