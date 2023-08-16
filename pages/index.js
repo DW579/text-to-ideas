e9e9bb24f0413cda745fb0ec0b1f178e6bbdaee7
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2023-08-16 13:23:56 -0700

    Implemented radio button type options

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,67 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
 
 export default function Home() {
+    const [item, setItem] = useState({ typeOfIdeas: "", another: "another" });
+
+    const { typeOfIdeas } = item;
+
+    const handleChange = (e) => {
+        e.persist();
+        console.log(e.target.value);
+
+        setItem((prevState) => ({
+            ...prevState,
+            typeOfIdeas: e.target.value,
+        }));
+    };
+
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <h1>Home page</h1>
+            <h1>Welcome to Text-to-Ideas!</h1>
+            <p>Upload a document or text and let the LLM figure what is the main issue within the text and present you with project ideas to solve the issue.</p>
+            <Form>
+                <Form.Group
+                    controlId="formFile"
+                    className="mb-3"
+                >
+                    <Form.Label>Upload document</Form.Label>
+                    <Form.Control type="file" />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                >
+                    <Form.Label>Copy and Paste if needed</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                    />
+                </Form.Group>
+                <Form.Group controlId="typeOfIdeas">
+                    <Form.Check
+                        value="large language model (llm)"
+                        type="radio"
+                        aria-label="radio 1"
+                        label="Large Language Model (LLM)"
+                        onChange={handleChange}
+                        checked={typeOfIdeas === "large language model (llm)"}
+                    />
+                    <Form.Check
+                        value="mechanical"
+                        type="radio"
+                        aria-label="radio 2"
+                        label="Mechanical"
+                        onChange={handleChange}
+                        checked={typeOfIdeas === "mechanical"}
+                    />
+                </Form.Group>
+            </Form>
         </Layout>
     );
 }
